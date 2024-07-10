1a7d097f7a602add3682c06e55381fe8174c67d2
moudiki lionel <lmoudiki99@gmail.com>
2024-07-10 16:57:49 +0200

    working on special offer section

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,19 +1,18 @@
 
 
-const ShoeCard = ( { imgURL, changBigShoeImage, bigShoeImg} ) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
   const handleClick = () => {
     if(bigShoeImg  !== imgURL.bigShoe){
-      changBigShoeImage(imgURL.bigShoe)
+      changeBigShoeImage(imgURL.bigShoe)
     }
   }
 
   return (
-    <div className={` border-2 rounded-xl 
-    ${bigShoeImg === imgURL.bigShoe
+    <div className={`border-2 rounded-xl 
+    ${ bigShoeImg === imgURL.bigShoe
       ? 'border-coral-red'
       : 'border-transparent' 
-    } cursor-pointer max-sm:flex-1
-    `} 
+    } cursor-pointer max-sm:flex-1`} 
     onClick={handleClick}
     >
       <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4 " >
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,10 +1,28 @@
-
+import { offer } from "../assets/images"
+import Button from "../components/Button"
 
 const SpecialOffer = () => {
   return (
-    <div>
-      Special Offer
-    </div>
+    <section className="flex justify-wrap  items-center max-xl:flex-col-reverse gap-10 max-container" >
+      <div className="flex-1" >
+        <img src={offer} alt="d"   width={733} height={687} className="w-full" />
+      </div>
+      <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg" >
+            We Provide You 
+        
+            <br />
+             <span className="text-coral-red " >Super 
+              </span>
+              <span className="text-coral-red"> Quality</span> Shoes
+          </h2>
+          <p className="mt-4 lg:max-w-lg info-text" >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel pariatur corporis, qui dolorem quia, consequuntur velit nihil error incidunt quidem odio temporibus
+          </p>
+          <p className="mt-6 lg:max-w-lg info-text" >Our dedication to detail and excellence ensures your satisfaction</p>
+          <div className="mt-11" > 
+          <Button label="View Details"  />
+          </div>
+    </section>
   )
 }
 
